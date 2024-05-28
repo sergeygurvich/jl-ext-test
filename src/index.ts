c2675d74fb932de6a1279ca0bd1b428375a75c52
sergeygurvich <segurvich@ucsd.edu>
2024-05-28 11:10:07 -0700

    Add styling, attribution, error handling

--- src/index.ts
+++ src/index.ts
@@ -34,6 +34,7 @@ const plugin: JupyterFrontEndPlugin<void> = {
     const newWidget = async () => {
       // Create a blank content widget inside a MainAreaWidget
       const content = new Widget();
+      content.addClass('my-apodWidget');
       const widget = new MainAreaWidget({ content });
       widget.id = 'apod-jupyterlab';
       widget.title.label = 'Astronomy Picture';
@@ -42,6 +43,9 @@ const plugin: JupyterFrontEndPlugin<void> = {
       // Add an image element to the content
       const img = document.createElement('img');
       content.node.appendChild(img);
+
+      const summary = document.createElement('p');
+      content.node.appendChild(summary);
 
       // Get a random date string in YYYY-MM-DD format
       function randomDate() {
@@ -57,14 +61,27 @@ const plugin: JupyterFrontEndPlugin<void> = {
       const response = await fetch(
         `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${randomDate()}`
       );
-      const data = (await response.json()) as APODResponse;
-
-      if (data.media_type === 'image') {
-        // Populate the image
-        img.src = data.url;
-        img.title = data.title;
+      if (!response.ok) {
+        const data = await response.json();
+        if (data.error) {
+          summary.innerText = data.error.message;
+        } else {
+          summary.innerText = response.statusText;
+        }
       } else {
-        console.log('Random APOD was not a picture.');
+        const data = (await response.json()) as APODResponse;
+
+        if (data.media_type === 'image') {
+          // Populate the image
+          img.src = data.url;
+          img.title = data.title;
+          summary.innerText = data.title;
+          if (data.copyright) {
+            summary.innerText += ` (Copyright ${data.copyright})`;
+          }
+        } else {
+          summary.innerText = 'Random APOD fetched was not an image.';
+        }
       }
       return widget;
     };
